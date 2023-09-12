1729b5a355eda91d7974e8bafc1b4410d7cd0a13
shreeya <1shreeya2@gmail.com>
2023-09-12 19:47:47 +0545

    working

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
