35487a59a4f1c0f2aa53c0b8c275b64e0a5b18c2
shreeya34 <1shreeya2@gmail.com>
2023-12-11 11:38:02 +0545

    working on forget password

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -10,11 +10,12 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Profile(models,Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     forget_password_token = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    objects = models.Manager()
    
     
     def __str__(self):
--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '1shreeya2@gmail.com'
-EMAIL_HOST_PASSWORD = 'f12sh0691'
+
 
